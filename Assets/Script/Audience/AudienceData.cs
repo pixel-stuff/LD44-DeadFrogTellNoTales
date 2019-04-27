c32f804ea4c9ebf3f06b68aa4cce57eb1c575765
Jesaipo <jeremy.morel.jm@gmail.com>
2019-04-27 19:14:45 +0200

    add Audience
    add Audience

---
+++ Assets/Script/Audience/AudienceData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Audience/Character", order = 1)]
+public class AudienceData : ScriptableObject
+{
+     public AnimationCurve weightCurve = AnimationCurve.Linear(0, 1, 10, 1);
+    public string objectName = "New Audience Character";
+
+    public Vector2 romanceNeeds;
+    public Vector2 epicnessNeeds;
+
+    public Sprite calmSprite;
+    public Sprite impacientSprite;
+    public Sprite angrySprite;
+}
