b6982fbcd8244a9d9d39bb7f29439a2d2c4697dd
Jesaipo <jeremy.morel.jm@gmail.com>
2019-04-28 23:57:25 +0200

    Add candle

--- Assets/TimerManager.cs
+++ Assets/TimerManager.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class TimerManager : MonoBehaviour
 {
     public MyStringEvent timerEvent;
+    public float maxTimeSecond = 30.0f;
     public float timeSecond = 30.0f;
+    public int pixelToMove = 100;
+    public GameObject Candle;
+
+    void Start()
+    {
+        timeSecond = maxTimeSecond;
+    }
 
     void Update()
     {
@@ -14,6 +22,10 @@ public class TimerManager : MonoBehaviour
             Debug.Log("GameOver");
         }
         timeSecond -= Time.deltaTime;
+        float timePercent = timeSecond / maxTimeSecond;
+        if (timePercent < 0)
+            timePercent = 0;
+        Candle.transform.localPosition = new Vector3(Candle.transform.localPosition.x, pixelToMove * timePercent, Candle.transform.localPosition.z);
         timerEvent.Invoke(((int)(timeSecond)).ToString());
     }
 }
