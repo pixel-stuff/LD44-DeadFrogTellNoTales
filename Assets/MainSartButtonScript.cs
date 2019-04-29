70eec1136a80c9cf04a55b060a64f2a9e0682a5d
Jesaipo <jeremy.morel.jm@gmail.com>
2019-04-29 20:31:57 +0200

    menu scene

---
+++ Assets/MainSartButtonScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSartButtonScript : MonoBehaviour
+{
+    public void NextLevel()
+    {
+        LoadScene("TutoScene");
+    }
+
+    void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+}
